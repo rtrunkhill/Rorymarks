6c901e4af83338e4d06308e7491fe6c00643285e
rtrunkhill <rtrunkhill@gmail.com>
2018-07-09 14:19:14 -0700

    Testing version on old branch

---
+++ RoryMarks/Models/Bookmarks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rorymarks.Models
+{
+    public class Bookmarks
+    {
+        public int BookmarkID { get; set; }
+        public string BookmarkAddress { get; set; }
+        public int TopicID { get; set; }
+
+        public virtual Topics Topics { get; set; }
+    }
+}
---
+++ RoryMarks/Models/Topics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rorymarks.Models
+{
+    public class Topics
+    {
+        public int TopicID { get; set; }
+        public string TopicTitle { get; set; }
+
+        public virtual ICollection<Bookmarks> Bookmarks { get; set; }
+    }
+}
